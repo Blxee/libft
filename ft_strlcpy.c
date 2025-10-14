6dadcec5cef45564ac6df27b2bc91dafc3420859
Amine Tahiri Alaoui <atahiri-@e1r5p10.1337.ma>
2025-10-14 13:24:54 +0100

    Updated: ft_strlcpy now uses ft_strlen

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,28 +6,18 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 09:54:17 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/14 10:23:18 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/14 13:24:22 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	helper_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	src_len;
 
-	src_len = helper_strlen(src);
+	src_len = ft_strlen(src);
 	if (size > 0)
 	{
 		i = 0;
