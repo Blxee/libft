ca49d29bab9c8c709f2de92c8269c5cdb452482d
Amine Tahiri Alaoui <atahiri-@e1r5p10.1337.ma>
2025-10-14 13:48:48 +0100

    Added: ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 13:27:04 by atahiri-          #+#    #+#             */
+/*   Updated: 2025/10/14 13:48:10 by atahiri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dstlen;
+	size_t	srclen;
+	size_t	i;
+
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if (size <= dstlen)
+		return (size + srclen);
+	i = 0;
+	while (dstlen + i < size - 1 && src[i] != '\0')
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dstlen + srclen);
+}
