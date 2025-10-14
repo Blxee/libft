3810193150df1af7c449ebb2af8c4a23d1913e3b
Amine Tahiri Alaoui <atahiri-@e1r5p2.1337.ma>
2025-10-14 09:22:44 +0100

    Added: ft_isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/14 08:41:05 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/14 09:22:26 by atahiri-         ###   ########.fr       */
+/*   Created: 2025/10/14 09:20:12 by atahiri-          #+#    #+#             */
+/*   Updated: 2025/10/14 09:22:04 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_islower(int c);
-int	ft_isupper(int c);
-int	ft_isprint(int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
+int	ft_isalnum(int c)
+{
+	int	islower;
+	int	isupper;
+	int	isdigit;
 
-#endif
+	islower = (c >= 'a' && c <= 'z');
+	isupper = (c >= 'A' && c <= 'Z');
+	isdigit = (c >= '0' && c <= '9');
+	return (islower || isupper || isdigit);
+}
