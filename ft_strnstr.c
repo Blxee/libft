1789ff703d7760cdf2c1408594a328831838182f
Amine Tahiri Alaoui <atahiri-@e1r5p10.1337.ma>
2025-10-14 14:51:03 +0100

    Added: ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 14:23:16 by atahiri-          #+#    #+#             */
+/*   Updated: 2025/10/14 14:50:33 by atahiri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < len && big[i] != '\0')
+	{
+		j = 0;
+		while (little[j] != '\0' && big[i + j] == little[j])
+		{
+			if (little[j + 1] == '\0')
+				return ((char *)(big + i));
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
