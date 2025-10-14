1284e24ab59a9cada861767f3dd7445acf0b1c06
Amine Tahiri Alaoui <atahiri-@e1r5p2.1337.ma>
2025-10-14 08:41:49 +0100

    Added: ft_isalpha.c

--- libft.h
+++ libft.h
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/14 08:33:04 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/14 08:40:51 by atahiri-         ###   ########.fr       */
+/*   Created: 2025/10/14 08:41:05 by atahiri-          #+#    #+#             */
+/*   Updated: 2025/10/14 08:41:14 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef LIBFT_H
+# define LIBFT_H
 
-int	ft_isalpha(int c)
-{
-	int	islower;
-	int	isupper;
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_islower(int c);
+int	ft_isupper(int c);
+int	ft_isprint(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
 
-	islower = (c >= 'a' && c <= 'z');
-	isupper = (c >= 'A' && c <= 'Z');
-	return (islower || isupper);
-}
+#endif
