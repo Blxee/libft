7bf1cd4958aebe7826479a05fdc743c76327621e
Amine Tahiri Alaoui <atahiri-@e1r3p15.1337.ma>
2025-10-15 08:13:39 +0100

    Added: ft_striteri

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 08:41:05 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/15 08:04:31 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/15 08:09:52 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
