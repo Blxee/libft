d27e495a67880053da3d496c20921cb6efedb1b2
Amine Tahiri Alaoui <atahiri-@e1r3p15.1337.ma>
2025-10-15 09:19:17 +0100

    Added: ft_lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:43:40 by atahiri-          #+#    #+#             */
+/*   Updated: 2025/10/15 08:46:09 by atahiri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (new == NULL)
+		return (NULL);
+	*new = (t_list){.content = content, .next = NULL};
+	return (new);
+}
