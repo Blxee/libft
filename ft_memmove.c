aafd49090247f664f254c869b507e57e7b73a839
Amine Tahiri Alaoui <atahiri-@e1r7p13.1337.ma>
2025-10-16 17:07:19 +0100

    Fixed: overflow error at ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 09:47:13 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/15 10:57:39 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/16 17:06:59 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*src_bytes;
 	size_t			i;
 
+	if (n == 0 || dest == NULL || src == NULL)
+		return (dest);
 	dest_bytes = (unsigned char *)dest;
 	src_bytes = (unsigned char *)src;
 	if (dest_bytes <= src_bytes)
 	{
 		i = 0;
-		while (i < n)
+		while (n--)
 		{
 			dest_bytes[i] = src_bytes[i];
 			i++;
@@ -32,7 +34,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	else
 	{
 		i = n - 1;
-		while (i >= 0)
+		while (n--)
 		{
 			dest_bytes[i] = src_bytes[i];
 			i--;
