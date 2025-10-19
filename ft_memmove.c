8a88e096b99ca11d2435f7d589f25dfeed1de805
Amine Tahiri Alaoui <atahiri-@e2r3p15.1337.ma>
2025-10-19 13:46:59 +0100

    Changed: ft_memmove now seg faults like the std ones

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 09:47:13 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/16 17:06:59 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/19 13:46:17 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*src_bytes;
 	size_t			i;
 
-	if (n == 0 || dest == NULL || src == NULL)
-		return (dest);
 	dest_bytes = (unsigned char *)dest;
 	src_bytes = (unsigned char *)src;
 	if (dest_bytes <= src_bytes)
