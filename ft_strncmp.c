15d233dc0ee70a024fa6b09cf0e14f97db72eadb
Amine Tahiri Alaoui <atahiri-@e2r2p13.1337.ma>
2025-10-21 10:52:47 +0100

    Fixed: ft_strncmp returning 0 when one of the strings end

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 14:05:09 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/16 20:11:48 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/21 10:52:02 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (n == 0 || s1 == NULL || s2 == NULL)
 		return (0);
 	i = 0;
-	while (i < n && s1[i] != '\0')
+	while (i < n)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == '\0' || s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
