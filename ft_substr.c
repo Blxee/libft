3ef12529c76c9af8462d5103d79731c925c0405b
Amine Tahiri Alaoui <atahiri-@e2r2p13.1337.ma>
2025-10-21 14:14:11 +0100

    Fixed: bugs in ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 15:56:36 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/21 12:08:35 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/21 14:13:26 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,25 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	size_t	s_len;
 	size_t	sub_len;
 	size_t	i;
 	char	*sub;
 
 	if (s == NULL)
 		return (NULL);
-	sub_len = ft_strlen(s);
+	s_len = ft_strlen(s);
+	sub_len = s_len;
 	if (len < sub_len)
 		sub_len = len;
 	sub = ft_calloc(sizeof(char), sub_len + 1);
-	if (start >= sub_len)
-		return (sub);
 	if (sub == NULL)
 		return (NULL);
 	i = 0;
-	while (i < sub_len && s[i] != '\0' && s[start + i] != '\0')
+	while (i < sub_len && start < s_len)
 	{
 		sub[i] = s[start + i];
 		i++;
 	}
-	sub[i] = '\0';
 	return (sub);
 }
