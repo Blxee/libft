c4d138e72b413b9106a9579322e44786c9488780
Amine Tahiri Alaoui <atahiri-@e2r2p13.1337.ma>
2025-10-21 10:58:34 +0100

    Fixed: ft_substr not allocating memory correctly

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 15:56:36 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/19 08:46:54 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/21 10:57:35 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	size_t	sub_len;
 	size_t	i;
 	char	*sub;
 
 	if (s == NULL)
 		return (NULL);
-	sub = malloc(len + 1);
+	sub_len = ft_strlen(s);
+	if (len < sub_len)
+		sub_len = len;
+	sub = malloc(sub_len + 1);
 	if (sub == NULL)
 		return (NULL);
 	i = 0;
-	while (i < len && s[i] != '\0' && s[start + i] != '\0')
+	while (i < sub_len && s[i] != '\0' && s[start + i] != '\0')
 	{
 		sub[i] = s[start + i];
 		i++;
