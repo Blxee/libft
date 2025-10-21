36e70f84c67d884a6c33b58625f0d98c73a8a3c8
Amine Tahiri Alaoui <atahiri-@e2r2p13.1337.ma>
2025-10-21 13:33:21 +0100

    Fixed: not comparing unsigned char in ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 14:05:09 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/21 10:52:02 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/21 13:32:43 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*ss1;
+	unsigned char	*ss2;
 
+	ss1 = (unsigned char *)s1;
+	ss2 = (unsigned char *)s2;
 	if (n == 0 || s1 == NULL || s2 == NULL)
 		return (0);
 	i = 0;
 	while (i < n)
 	{
-		if (s1[i] == '\0' || s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (ss1[i] == '\0' || ss1[i] != ss2[i])
+			return (ss1[i] - ss2[i]);
 		i++;
 	}
 	return (0);
