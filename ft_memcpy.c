541705b34a2fea13bd80986dc328af82887f099e
Amine Tahiri Alaoui <atahiri-@e2r3p15.1337.ma>
2025-10-24 08:14:56 +0100

    Fixed: ft_memcpy not returning dest when one of args is null

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 09:43:15 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/22 11:51:02 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/24 08:14:26 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	if (dest == NULL || src == NULL)
-		return (NULL);
+		return (dest);
 	i = 0;
 	while (i < n)
 	{
